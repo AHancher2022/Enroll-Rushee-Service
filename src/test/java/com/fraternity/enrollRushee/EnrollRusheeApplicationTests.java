3c63d09ac4db6459adeb545a2a186bd98abb6c60
Aj Hancher <ajhancher@Ajs-MacBook-Pro.local>
2022-02-08 20:09:12 -0500

    first commit

---
+++ src/test/java/com/fraternity/enrollRushee/EnrollRusheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fraternity.enrollRushee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollRusheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
