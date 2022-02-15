07c04c121311a2ef69e0c1e45263119451ed5b00
AJ Hancher <ajh0067@auburn.edu>
2022-02-15 12:45:39 -0500

    First edition for authenitcation api

---
+++ src/main/java/com/fraternity/enrollRushee/model/Message.java
@@ -0,0 +1,14 @@
+package com.fraternity.enrollRushee.model;
+
+
+public class Message {
+ private final String message;
+
+ public Message(String message) {
+     this.message = message;
+ }
+
+ public String getMessage() {
+     return this.message;
+ }
+}
